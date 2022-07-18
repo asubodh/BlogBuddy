92dd4a2ddb0d142abf61c099cd5bd5941090d081
Subodh Aryal <43497160+asubodh@users.noreply.github.com>
2022-07-18 21:48:22 +0545

    Added Search On YouTube!

---
+++ Events/ytsearch.js
@@ -0,0 +1,13 @@
+﻿chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'Youtube Search - BlogBuddy',
+    title: 'Search YouTube for \'%s\'',
+    type: 'normal',
+    contexts: ['selection']
+  });
+});
+chrome.contextMenus.onClicked.addListener((info) => {
+  chrome.tabs.create({
+    url: 'http://youtube.com/results?search_query='+encodeURIComponent(info.selectionText)
+  });
+});
